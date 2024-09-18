80831c4389c079f12efff8b9400424dba510c36b
Jeongho Kim <kimjeongho466@gmail.com>
2024-09-18 22:41:46 +0900

    third commit

--- src/Report/ArrayListReport2.java
+++ src/Report/ArrayListReport2.java
@@ -24,13 +24,22 @@ public class ArrayListReport2 {
             list2.add(input); //입력 받은 문자열을 list2에 추가
         }
 
+        System.out.print("List: ");
+        for(String s : list)
+            System.out.print(s + " ");
+        System.out.println(" ");
+        System.out.print("List2: ");
+        for(String s : list2)
+            System.out.print(s + " ");
+
         //list - list2 하는 코드
         for (int i = 0; i < list2.size(); i++)
             for(int j = 0; j < list.size(); j++){
                 if(list2.get(i).equals(list.get(j))) //문자열 비교를 위해 equals 사용
                     list.remove(j);                 //list2에서 list를 비교하며 같은 문자열이면 해당 문자열을 list에서 삭제하는 방법
             }
-        
+        System.out.println(" ");
+        System.out.print("List - List2: ");
         for(String s : list)
             System.out.print(s + " "); //list2와 겹치는 문자열을 삭제한 list를 출력
     }
